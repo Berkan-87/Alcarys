52d73c02e9d06a45d56acf4e4ae44c39460fe4d4
Berkan-87 <sby.notess@gmail.com>
2025-07-25 14:11:13 +0300

    eklendi

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,22 +2,22 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const JWT_SECRET = 'gizli_tarot_anahtari'; // .env'e taşı istersen
+const JWT_SECRET = process.env.JWT_SECRET || 'gizli_tarot_anahtari'; // .env kullanman iyi olur
 
 // REGISTER
 const register = async (req, res) => {
-  const { name, surname, email, password } = req.body;
+  const { name, email, password } = req.body;  // surname kaldırıldı
 
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser)
+    if (existingUser) {
       return res.status(400).json({ message: 'Bu email zaten kullanılıyor' });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       name,
-      surname,
       email,
       password: hashedPassword,
     });
@@ -37,16 +37,21 @@ const login = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user)
+    if (!user) {
       return res.status(400).json({ message: 'Geçersiz email veya şifre' });
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
+    if (!isMatch) {
       return res.status(400).json({ message: 'Geçersiz email veya şifre' });
+    }
 
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1d' });
 
-    res.status(200).json({ token, user: { id: user._id, name: user.name, surname: user.surname, email: user.email } });
+    res.status(200).json({
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Sunucu hatası' });
